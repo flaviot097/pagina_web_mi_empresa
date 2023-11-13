2dc6ce1c4b582639c3403f201c96683550ee863c
flaviot097 <112975530+flaviot097@users.noreply.github.com>
2023-11-13 03:12:32 -0300

    ahora los formularios de creacion e inicio son dinamicos

--- src/scrips/abilitar_crear_usuario.js
+++ src/scrips/abilitar_crear_usuario.js
@@ -1,21 +1,10 @@
-const login = document.querySelector(".card-login");
+document.getElementById("crear-usuario").style.display = "none";
 
-const createcard = document.querySelector(".card-login-create");
+const btnCrear = document.getElementById("btn-crear-usuario");
 
-console.log(login);
-
-const btnCrearUsuario = document.querySelector("#btn-crear-usuario");
-const displayCartas = (login.style.display = "block");
-
-const ruta = "http://127.0.0.1:5500/login.html";
-
-console.log(login.style.display);
-
-function cambiarDiv() {
-  login.style.display = "none";
-  createcard.style.display = "block";
+function mostrarDiv() {
+  document.getElementById("logear-usuario").style.display = "none";
+  document.getElementById("crear-usuario").style.display = "";
 }
 
-btnCrearUsuario.addEventListener("Click", cambiarDiv());
-
-//card - login - ocultar.card - login - create - mostrar;
+btnCrear.addEventListener("click", mostrarDiv);
