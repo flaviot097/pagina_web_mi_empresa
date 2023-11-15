fc384bb7e83d753d76866cf3d35af91895d778f0
flaviot097 <112975530+flaviot097@users.noreply.github.com>
2023-11-15 12:39:33 -0300

    agregando funcionalidad agregar al carrito

---
+++ src/scrips/servicios.js
@@ -0,0 +1,105 @@
+const divcarrito = document.getElementById("contenedor_tarjetas_inicio");
+const servicios = [
+  {
+    id: "0",
+    nombre_y_apellido: "teodoro ajia",
+    categoria_servicio: "plomero",
+    descripcion: "saddasdsadsad sadsadsad",
+    nombre_servicio: "teordoro el inodoro",
+    precio: 32323,
+    fecha: "2023-11-05T03:00:00.000Z",
+  },
+  {
+    id: "1",
+    nombre_y_apellido: "juan mecanico",
+    categoria_servicio: "mecanico",
+    descripcion: "saddasdsadsad sadsadsad",
+    nombre_servicio: "mecajuan",
+    precio: 567,
+    fecha: "2023-11-05T03:00:00.000Z",
+  },
+  {
+    id: "2",
+    nombre_y_apellido: "eketencio ramires",
+    categoria_servicio: "plomero",
+    descripcion: "sagrgrgrgrg",
+    nombre_servicio: "ramires serv",
+    precio: 23467,
+    fecha: "2023-11-05T03:00:00.000Z",
+  },
+  {
+    id: "3",
+    nombre_y_apellido: "eketencio ramires",
+    categoria_servicio: "plomero",
+    descripcion: "sagrgrgrgrg",
+    nombre_servicio: "ramires serv",
+    precio: 23467,
+    fecha: "2023-11-05T03:00:00.000Z",
+  },
+  {
+    id: "4",
+    nombre_y_apellido: "eketencio ramires",
+    categoria_servicio: "plomero",
+    descripcion: "sagrgrgrgrg",
+    nombre_servicio: "ramires serv",
+    precio: 23467,
+    fecha: "2023-11-05T03:00:00.000Z",
+  },
+  {
+    id: "5",
+    nombre_y_apellido: "eketencio ramires",
+    categoria_servicio: "plomero",
+    descripcion: "sagrgrgrgrg",
+    nombre_servicio: "ramires serv",
+    precio: 23467,
+    fecha: "2023-11-05T03:00:00.000Z",
+  },
+  {
+    id: "6",
+    nombre_y_apellido: "eketencio ramires",
+    categoria_servicio: "plomero",
+    descripcion: "sagrgrgrgrg",
+    nombre_servicio: "ramires serv",
+    precio: 23467,
+    fecha: "2023-11-05T03:00:00.000Z",
+  },
+  {
+    id: "7",
+    nombre_y_apellido: "eketencio ramires",
+    categoria_servicio: "plomero",
+    descripcion: "sagrgrgrgrg",
+    nombre_servicio: "ramires serv",
+    precio: 1,
+    fecha: "2023-11-05T03:00:00.000Z",
+  },
+];
+
+//console.log(divcarrito);
+//divcarrito.innerHTML = "";
+
+function agregarServicios(datos) {
+  datos.forEach((dato) => {
+    let nuevodiv = `<div class="card" style="width: 18rem">
+        <img src="./img/car-wash-1619823_1280.jpg" class="card-img-top" alt="..." />
+        <div class="card-body">
+        <h5 class="card-title">${dato.nombre_servicio}</h5>
+        <p class="card-text">${dato.descripcion}.</p>
+        <p class="card-text"><b>$${dato.precio}</b></p>
+        <a href="#" class="btn btn-primary" id="${dato.id}">🛒</a>
+        </div>
+    </div>`;
+    let nuevohtml = (divcarrito.innerHTML += nuevodiv);
+    return nuevohtml;
+  });
+}
+
+agregarServicios(servicios);
+
+const btnTarjetaSev = document.querySelectorAll(".card");
+console.log(btnTarjetaSev);
+
+const agragarCarrito = () => {
+  let arrayCarrito = [];
+};
+
+//exports.module = { servicios };
