7a9a6a450cfc3067b670a6b1ac5035cd2c7c1a20
flaviot097 <112975530+flaviot097@users.noreply.github.com>
2023-11-21 14:58:41 -0300

    mejorando estilos

--- src/scrips/tarjeta_servicio.js
+++ src/scrips/tarjeta_servicio.js
@@ -5,13 +5,13 @@ const datos = JSON.parse(localStorage.getItem("servicio"));
 /// consulto el servicio por id
 
 const div = `<div class="card-servicio">
-<img src="./img/car-wash-1619823_1280.jpg" class="card-img-top" alt="..." />
+<img src="./img/car-wash-1619823_1280.jpg" class="card-img-top serv" alt="..." />
 <div class="card-body">
 <h5 class="card-title">${datos.nombre_servicio}</h5>
 <p class="card-text">${datos.descripcion}</p>
 <p class="card-text"><b>Precio: $${datos.precio}</b></p>
 </div>
 </div>
-<a class="btn btn-primary" name="" id="">🛒</a>`;
+<a class="btn btn-primary serv" name="" id="">🛒</a>`;
 
 tarjeta.innerHTML += div;
